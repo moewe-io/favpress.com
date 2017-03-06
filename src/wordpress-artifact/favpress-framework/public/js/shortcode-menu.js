5f0434af7bdc832b4ad5b1ae25407c3371995c22
Adrian Mörchen <adrian.moerchen@moewe.io>
2017-03-06 09:38:31 +0100

    Fixed wrong jquery parameter

--- src/wordpress-artifact/favpress-framework/public/js/shortcode-menu.js
+++ src/wordpress-artifact/favpress-framework/public/js/shortcode-menu.js
@@ -48,71 +48,71 @@ jQuery(function (jQuery) {
     })();
 
     jQuery.fn.scReset = function () {
-        if ($(this).is('form'))
-            $(this)[0].reset();
+        if (jQuery(this).is('form'))
+            jQuery(this)[0].reset();
 
-        $(this).find('.favpress-sc-field').each(function (i) {
-            var type = $(this).attr('data-favpress-type');
+        jQuery(this).find('.favpress-sc-field').each(function (i) {
+            var type = jQuery(this).attr('data-favpress-type');
             switch (type) {
                 case 'favpress-upload':
-                    $(this).find('.image > img').attr('src', '');
+                    jQuery(this).find('.image > img').attr('src', '');
                     break;
                 case 'favpress-color':
-                    $(this).find('.favpress-js-colorpicker').colorpicker('update', '');
+                    jQuery(this).find('.favpress-js-colorpicker').colorpicker('update', '');
                     break;
                 case 'favpress-slider':
-                    var val = $(this).find('input').val();
-                    var $slider = $(this).find('.favpress-js-slider');
+                    var val = jQuery(this).find('input').val();
+                    var $slider = jQuery(this).find('.favpress-js-slider');
                     if (!val) {
-                        val = $(this).find('.favpress-js-slider').slider('option', 'min');
+                        val = jQuery(this).find('.favpress-js-slider').slider('option', 'min');
                     }
                     $slider.slider('value', val);
                     break;
                 case 'favpress-textarea':
-                    $(this).find('textarea').val($(this).find('textarea').text());
+                    jQuery(this).find('textarea').val(jQuery(this).find('textarea').text());
                     break;
                 case 'favpress-checkimage':
                     // trigger change since form reset doesn't trigger it
-                    $(this).find('input').change();
+                    jQuery(this).find('input').change();
                     break;
                 case 'favpress-radioimage':
                     // trigger change since form reset doesn't trigger it
-                    $(this).find('input').change();
+                    jQuery(this).find('input').change();
                     break;
                 case 'favpress-codeeditor':
-                    $(this).find('textarea').first().val($(this).find('textarea').text()).change();
+                    jQuery(this).find('textarea').first().val(jQuery(this).find('textarea').text()).change();
                     break;
             }
         });
 
         if ($.fn.select2) {
             // re-init select2
-            $(this).find('.favpress-js-select2').select2("destroy");
+            jQuery(this).find('.favpress-js-select2').select2("destroy");
             // re-init select2 sortable
-            if ($.fn.select2Sortable) $(this).find('.favpress-js-sorter').select2("destroy");
+            if ($.fn.select2Sortable) jQuery(this).find('.favpress-js-sorter').select2("destroy");
             // re-init select2 fontawesome
-            $(this).find('.favpress-js-fontawesome').select2("destroy");
+            jQuery(this).find('.favpress-js-fontawesome').select2("destroy");
 
-            favpress.init_fontawesome_chooser($(this).find('.favpress-js-fontawesome'));
-            favpress.init_select2($(this).find('.favpress-js-select2'));
-            favpress.init_sorter($(this).find('.favpress-js-sorter'));
+            favpress.init_fontawesome_chooser(jQuery(this).find('.favpress-js-fontawesome'));
+            favpress.init_select2(jQuery(this).find('.favpress-js-select2'));
+            favpress.init_sorter(jQuery(this).find('.favpress-js-sorter'));
         }
-        return $(this);
+        return jQuery(this);
     };
 
     // init controls
-    favpress.init_controls($('.favpress-sc-main'));
+    favpress.init_controls(jQuery('.favpress-sc-main'));
 
     // shortcode image controls event bind
     favpress.custom_check_radio_event(".favpress-sc-dialog", ".favpress-checkimage .field .input label");
     favpress.custom_check_radio_event(".favpress-sc-dialog", ".favpress-radioimage .field .input label");
 
-    $('.favpress-sc-menu li a').on('click', function (ev) {
+    jQuery('.favpress-sc-menu li a').on('click', function (ev) {
         ev.preventDefault();
 
-        var $modal = $(this).parents('.favpress-sc-dialog'),
-            $parent = $(this).parent('li'),
-            targetId = $(this).attr('href').substring(1),
+        var $modal = jQuery(this).parents('.favpress-sc-dialog'),
+            $parent = jQuery(this).parent('li'),
+            targetId = jQuery(this).attr('href').substring(1),
             $target = $modal.find('.favpress-sc-sub-menu-' + targetId);
 
         // set clicked menu item as current
@@ -127,11 +127,11 @@ jQuery(function (jQuery) {
         return false;
     });
 
-    $('.favpress-sc-element .favpress-sc-element-heading').unbind();
-    $('.favpress-sc-element .favpress-sc-element-heading a').bind('click.favpress_sc', function (e) {
+    jQuery('.favpress-sc-element .favpress-sc-element-heading').unbind();
+    jQuery('.favpress-sc-element .favpress-sc-element-heading a').bind('click.favpress_sc', function (e) {
         e.preventDefault();
 
-        var $parent = $(this).parents('li'),
+        var $parent = jQuery(this).parents('li'),
             id = $parent.attr('id'),
             $form = $parent.find('.favpress-sc-element-form').first();
 
@@ -142,7 +142,7 @@ jQuery(function (jQuery) {
         }
         else {
             var code = $parent.find('.favpress-sc-code').first().html(),
-                $modal = $(this).parents('.favpress-sc-dialog').first();
+                $modal = jQuery(this).parents('.favpress-sc-dialog').first();
 
             $modal.find('.favpress-sc-element').removeClass('active');
             $modal.find('.favpress-sc-element-form').favpress_slideUp();
@@ -159,11 +159,11 @@ jQuery(function (jQuery) {
         }
     });
 
-    $('.favpress-sc-insert').bind('click.favpress_sc_insert', function (e) {
+    jQuery('.favpress-sc-insert').bind('click.favpress_sc_insert', function (e) {
         e.preventDefault();
-        var $modal = $(this).parents('.favpress-sc-dialog'),
-            $parent = $(this).parents('.favpress-sc-element'),
-            $form = $(this).parents('.favpress-sc-element-form'),
+        var $modal = jQuery(this).parents('.favpress-sc-dialog'),
+            $parent = jQuery(this).parents('.favpress-sc-element'),
+            $form = jQuery(this).parents('.favpress-sc-element-form'),
             $fields = $form.find('.favpress-sc-field'),
             values = {},
             code = $parent.find('.favpress-sc-code').first().html(),
@@ -178,10 +178,10 @@ jQuery(function (jQuery) {
 
         // gather unique names of the options
         $fields.each(function (i) {
-            var $input = $(this).find(':not(div).favpress-input'),
+            var $input = jQuery(this).find(':not(div).favpress-input'),
                 name = $input.attr('name'),
                 val = $input.validationVal(),
-                type = $(this).attr('data-favpress-type');
+                type = jQuery(this).attr('data-favpress-type');
 
             if (type === 'favpress-toggle') {
                 if (val) val = 'true';
@@ -205,16 +205,16 @@ jQuery(function (jQuery) {
         $modal.trigger('favpress_insert_shortcode', code);
 
         // reset form and close dialog
-        $('.favpress-sc-element').removeClass('active');
+        jQuery('.favpress-sc-element').removeClass('active');
         $form.favpress_slideUp();
         $form.scReset();
         $modal.trigger('reveal:close');
     });
 
-    $('.favpress-sc-cancel').bind('click.favpress_sc_cancel', function (e) {
+    jQuery('.favpress-sc-cancel').bind('click.favpress_sc_cancel', function (e) {
         e.preventDefault();
-        $('.favpress-sc-element').removeClass('active');
-        var $form = $(this).parents('.favpress-sc-element-form')
+        jQuery('.favpress-sc-element').removeClass('active');
+        var $form = jQuery(this).parents('.favpress-sc-element-form');
         $form.favpress_slideUp();
         $form.scReset();
     });
