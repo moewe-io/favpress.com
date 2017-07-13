361fb922710db3e02f5289daee678ac806f58337
Adrian Mörchen <adrian.moerchen@moewe.io>
2017-07-13 13:33:45 +0200

    Fixed warnings.

--- src/wordpress-artifact/favpress-framework/views/control/checkbox.php
+++ src/wordpress-artifact/favpress-framework/views/control/checkbox.php
@@ -1,7 +1,7 @@
 <?php if(!$is_compact) echo FavPress_View::instance()->load('control/template_control_head', $head_info); ?>
 <?php foreach ($items as $item): ?>
 <label>
-	<?php $checked = (in_array($item->value, $value)); ?>
+	<?php $checked = is_array($value) && in_array($item->value, $value); ?>
 	<input <?php if($checked) echo 'checked'; ?> class="favpress-input<?php if($checked) echo " checked"; ?>" type="checkbox" name="<?php echo $name; ?>" value="<?php echo $item->value; ?>" />
 	<span></span><?php echo $item->label; ?>
 </label>
--- src/wordpress-artifact/favpress-framework/views/control/multiselect.php
+++ src/wordpress-artifact/favpress-framework/views/control/multiselect.php
@@ -1,9 +1,11 @@
-<?php if(!$is_compact) echo FavPress_View::instance()->load('control/template_control_head', $head_info); ?>
+<?php if (!$is_compact) echo FavPress_View::instance()->load('control/template_control_head', $head_info); ?>
 
-<select multiple name="<?php echo $name; ?>" class="favpress-input favpress-js-select2" autocomplete="off">
-	<?php foreach ($items as $item): ?>
-	<option <?php if(in_array($item->value, $value)) echo "selected" ?> value="<?php echo $item->value; ?>"><?php echo $item->label; ?></option>
-	<?php endforeach; ?>
-</select>
+    <select multiple name="<?php echo $name; ?>" class="favpress-input favpress-js-select2" autocomplete="off">
+        <?php foreach ($items as $item): ?>
+            <option <?php if (is_array($value) && in_array($item->value, $value)) {
+                echo "selected";
+            } ?> value="<?php echo $item->value; ?>"><?php echo $item->label; ?></option>
+        <?php endforeach; ?>
+    </select>
 
-<?php if(!$is_compact) echo FavPress_View::instance()->load('control/template_control_foot', $head_info); ?>
+<?php if (!$is_compact) echo FavPress_View::instance()->load('control/template_control_foot', $head_info); ?>
