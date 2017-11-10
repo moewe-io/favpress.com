5d6d513818f539ad364ca146811415a45e93aad5
Adrian Mörchen <adrian.moerchen@moewe.io>
2017-11-10 16:29:01 +0100

    Fixed some PHP warnings.

--- src/wordpress-artifact/favpress-framework/views/control/checkbox.php
+++ src/wordpress-artifact/favpress-framework/views/control/checkbox.php
@@ -1,7 +1,7 @@
 <?php if(!$is_compact) echo FavPress_View::instance()->load('control/template_control_head', $head_info); ?>
 <?php foreach ($items as $item): ?>
 <label>
-	<?php $checked = (in_array($item->value, $value)); ?>
+	<?php $checked = (in_array($item->value, is_array($value) ? $value : array($value))); ?>
 	<input <?php if($checked) echo 'checked'; ?> class="favpress-input<?php if($checked) echo " checked"; ?>" type="checkbox" name="<?php echo $name; ?>" value="<?php echo $item->value; ?>" />
 	<span></span><?php echo $item->label; ?>
 </label>
