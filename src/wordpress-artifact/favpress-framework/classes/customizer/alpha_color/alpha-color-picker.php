b50d0df43202a63005f53ad846c099339550ca24
Adrian Mörchen <adrian.moerchen@moewe.io>
2017-12-19 17:21:20 +0100

    Update Color Picker to be compatible with latest WordPress

--- src/wordpress-artifact/favpress-framework/classes/customizer/alpha_color/alpha-color-picker.php
+++ src/wordpress-artifact/favpress-framework/classes/customizer/alpha_color/alpha-color-picker.php
@@ -21,73 +21,83 @@
  */
 class Customize_Alpha_Color_Control extends WP_Customize_Control {
 
-	/**
-	 * Official control name.
-	 */
-	public $type = 'alpha-color';
+    /**
+     * Official control name.
+     */
+    public $type = 'alpha-color';
 
-	/**
-	 * Add support for palettes to be passed in.
-	 *
-	 * Supported palette values are true, false, or an array of RGBa and Hex colors.
-	 */
-	public $palette;
+    /**
+     * Add support for palettes to be passed in.
+     *
+     * Supported palette values are true, false, or an array of RGBa and Hex colors.
+     */
+    public $palette;
 
-	/**
-	 * Add support for showing the opacity value on the slider handle.
-	 */
-	public $show_opacity;
+    /**
+     * Add support for showing the opacity value on the slider handle.
+     */
+    public $show_opacity;
 
-	/**
-	 * Enqueue scripts and styles.
-	 *
-	 * Ideally these would get registered and given proper paths before this control object
-	 * gets initialized, then we could simply enqueue them here, but for completeness as a
-	 * stand alone class we'll register and enqueue them here.
-	 */
-	public function enqueue() {
-		wp_enqueue_script(
-			'alpha-color-picker',
-            FAVPRESS_URL . '/classes/customizer/alpha_color/alpha-color-picker.js',
-			array( 'jquery', 'wp-color-picker' ),
-			'1.0.0',
-			true
-		);
-		wp_enqueue_style(
-			'alpha-color-picker',
-            FAVPRESS_URL . '/classes/customizer/alpha_color/alpha-color-picker.css',
-			array( 'wp-color-picker' ),
-			'1.0.0'
-		);
-	}
+    /**
+     * Enqueue scripts and styles.
+     *
+     * Ideally these would get registered and given proper paths before this control object
+     * gets initialized, then we could simply enqueue them here, but for completeness as a
+     * stand alone class we'll register and enqueue them here.
+     */
+    public function enqueue() {
 
-	/**
-	 * Render the control.
-	 */
-	public function render_content() {
+        $default_script_location = get_stylesheet_directory_uri() . '/admin/customizer/alpha-color-picker/alpha-color-picker.js';
 
-		// Process the palette
-		if ( is_array( $this->palette ) ) {
-			$palette = implode( '|', $this->palette );
-		} else {
-			// Default to true.
-			$palette = ( false === $this->palette || 'false' === $this->palette ) ? 'false' : 'true';
-		}
+        wp_enqueue_script(
+            'alpha-color-picker',
+            apply_filters('alpha-color-picker-scripts', $default_script_location),
+            array( 'jquery', 'wp-color-picker' ),
+            '1.0.0',
+            true
+        );
 
-		// Support passing show_opacity as string or boolean. Default to true.
-		$show_opacity = ( false === $this->show_opacity || 'false' === $this->show_opacity ) ? 'false' : 'true';
+        $default_styles_location = get_stylesheet_directory_uri() . '/admin/customizer/alpha-color-picker/alpha-color-picker.css';
 
-		// Begin the output. ?>
-		<label>
-			<?php // Output the label and description if they were passed in.
-			if ( isset( $this->label ) && '' !== $this->label ) {
-				echo '<span class="customize-control-title">' . sanitize_text_field( $this->label ) . '</span>';
-			}
-			if ( isset( $this->description ) && '' !== $this->description ) {
-				echo '<span class="description customize-control-description">' . sanitize_text_field( $this->description ) . '</span>';
-			} ?>
-			<input class="alpha-color-control" type="text" data-show-opacity="<?php echo $show_opacity; ?>" data-palette="<?php echo esc_attr( $palette ); ?>" data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?>  />
-		</label>
-		<?php
-	}
+        wp_enqueue_style(
+            'alpha-color-picker',
+            apply_filters('alpha-color-picker-styles', $default_styles_location),
+            array( 'wp-color-picker' ),
+            '1.0.0'
+        );
+    }
+
+    /**
+     * Render the control.
+     */
+    public function render_content() {
+
+        // Process the palette
+        if ( is_array( $this->palette ) ) {
+            $palette = implode( '|', $this->palette );
+        } else {
+            // Default to true
+            $palette = ( false === $this->palette || 'false' === $this->palette ) ? 'false' : 'true';
+        }
+
+        // Support passing show_opacity as string or boolean. Default to true.
+        $show_opacity = ( false === $this->show_opacity || 'false' === $this->show_opacity ) ? 'false' : 'true';
+
+        // Output the label if passed in
+        if ( isset( $this->label ) && '' !== $this->label ) {
+            echo '<span class="customize-control-title">' . sanitize_text_field( $this->label ) . '</span>';
+        }
+
+        // Output the description if passed in
+        if ( isset( $this->description ) && '' !== $this->description ) {
+            echo '<span class="description customize-control-description">' . sanitize_text_field( $this->description ) . '</span>';
+        }
+        ?>
+        <div class="customize-control-content">
+            <label>
+                <input class="alpha-color-control" type="text" data-show-opacity="<?php echo $show_opacity; ?>" data-palette="<?php echo esc_attr( $palette ); ?>" data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?>  />
+            </label>
+        </div>
+        <?php
+    }
 }
